8b644b4230997e1bac8f3c39d6aaca9885497eb8
Jack O'Hara <jack.ohara@sorted.com>
2020-09-12 09:48:56 +0100

    Some refactoring to move out the form to its own component and change all the css into styled components

--- src/App.js
+++ src/App.js
@@ -1,90 +1,33 @@
-import React, { useState, useEffect } from "react";
-import "./App.css";
+import React from 'react';
+import styled from 'styled-components';
+import './App.css';
+import { WeightInputForm } from './components/weight-input-form/weightInputForm';
 
-function App() {
-  const [date, setDate] = useState("");
-  const [weightValue, setWeightValue] = useState(0.0);
-  const [weightUnit, setWeightUnit] = useState(
-    localStorage.getItem("weightUnit")
-  );
+const Container = styled.div`
+  text-align: center;
+  min-height: 100vh;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  background-color: #282c34;
+  color: white;
+`;
 
-  useEffect(() => {
-    let weights = localStorage.getItem("weights");
+const AppHeader = styled.header`
+  font-size: calc(10px + 2vmin);
+`;
 
-    if (!weights) {
-      localStorage.setItem("weights", JSON.stringify({}));
-    }
-
-    //setDate(getTodaysDate());
-  }, []);
-
-  // const getTodaysDate = () => {
-  //   const date = new Date();
-
-  //   return `${date.getFullYear()}-${date.getMonth() + 1}-${date.getDate()}`;
-  // };
-
-  const onSubmit = (event) => {
-    event.preventDefault();
-
-    let kgValue = weightValue;
-
-    if (weightUnit === "lb") {
-      kgValue = weightValue / 2.20462;
-    }
-
-    const weights = JSON.parse(localStorage.getItem("weights"));
-    weights[date] = kgValue;
-
-    localStorage.setItem("weights", JSON.stringify(weights));
-
-    alert("Submitted!");
-  };
-
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <h1>Weight Tracker</h1>
-      </header>
-      <form className="weight-input" onSubmit={onSubmit}>
-        <label htmlFor="weight">Input Weight:</label>
-        <div className="inputs">
-          <input
-            type="date"
-            id="date"
-            name="date"
-            value={date}
-            onChange={(e) => setDate(e.target.value)}
-          />
-          <div className="weight-select">
-            <input
-              type="number"
-              step="0.1"
-              name="weight"
-              id="weight"
-              value={weightValue}
-              onChange={(e) => {
-                setWeightValue(e.target.value);
-              }}
-            />
-            <select
-              name="unit"
-              id="unit"
-              value={weightUnit}
-              onChange={(e) => {
-                setWeightUnit(e.target.value);
-                localStorage.setItem("weightUnit", e.target.value);
-              }}>
-              <option value="kg">Kg</option>
-              <option value="lb">lb</option>
-            </select>
-          </div>
-        </div>
+    <Container>
+      <AppHeader className="App-header">
+        <h1>Jack's Weight Tracker</h1>
+      </AppHeader>
 
-        <input type="submit" value="Submit" />
-      </form>
-    </div>
+      <WeightInputForm />
+    </Container>
   );
-}
+};
 
 export default App;
